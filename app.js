200e041321fffd4f7bda0ce3527e260e6a3736bb
shailu-18 <rooparoopa786000@gmail.com>
2025-07-18 20:52:17 +0530

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
-const PORT = 5000;
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -15,7 +15,9 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use('/api/users', userRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
-    console.log(`Server is Running : http://localhost:${PORT}`);
+    console.log(`Server is Running http:localhost:${PORT}`);
 });
